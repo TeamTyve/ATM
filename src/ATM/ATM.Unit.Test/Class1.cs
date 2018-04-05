0b9701cef64ee18ed6996d01196447b6b625c8a6
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-04-05 11:22:10 +0200

    Added a simple test

--- src/ATM/ATM.Unit.Test/Class1.cs
+++ src/ATM/ATM.Unit.Test/Class1.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace ATM.Unit.Test
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void Test()
+        {
+            Assert.True(true);
+        }
     }
 }
