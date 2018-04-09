3abe612a35bfd600996e283fcc6c7c2ef4076aa8
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-04-09 16:02:56 +0200

    Jenkins Friendly Test, made compatilbe with local

--- src/ATM/ATM.Unit.Test/OutPutTest.cs
+++ src/ATM/ATM.Unit.Test/OutPutTest.cs
@@ -14,7 +14,6 @@ namespace ATM.Unit.Test
         [Test]
         public void Output_Format_Text()
         {
-            var dateTime = System.DateTime.Now;
             var uut = new Output
             {
                 ConsoleOut = Substitute.For<IConsoleOut>()
@@ -25,7 +24,7 @@ namespace ATM.Unit.Test
                 new Track("Tag;0;0;0;00010101010101001")
             });
 
-            uut.ConsoleOut.Received(1).WriteLine("Tag:Tag | Altitude:0 | x:0, y:0 | Timestamp:01-01-0001 01:01:01.1");
+            uut.ConsoleOut.Received(1).WriteLine("Track Objectification Software");
         }
     }
 }
