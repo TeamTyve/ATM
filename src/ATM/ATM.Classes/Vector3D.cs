d71363691aa8f94f792aa8092272f26b0585afd6
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-04-17 09:02:29 +0200

    Vector and AirSpace

---
+++ src/ATM/ATM.Classes/Vector3D.cs
@@ -0,0 +1,17 @@
+﻿namespace ATM.Classes
+{
+    public class Vector3D : Vector2D
+    {
+        public int Z { get; set; }
+
+        public Vector3D(int x = 0, int y = 0, int z = 0) : base(x, y)
+        {
+            Z = z;
+        }
+
+        public override bool IsPositive()
+        {
+            return base.IsPositive() && Z >= 0;
+        }
+    }
+}
