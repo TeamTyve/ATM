d55b818be390c76bf7d237db8cfa4421620b537c
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-04-23 14:01:39 +0200

    Fixed Stuff

---
+++ src/ATM/ATM.Unit.Test/Boundary/ConsoleLogger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using ATM.Classes;
+using ATM.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ATM.Unit.Test.Boundary
+{
+    [TestFixture]
+    public class ConsoleLoggerUnitTest
+    {
+        [Test]
+        public void ConsoleLogger_Write_WriteMessage()
+        {
+            var uut = new Output();
+
+        }
+    }
+}
