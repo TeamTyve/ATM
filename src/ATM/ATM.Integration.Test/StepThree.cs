5ecdd2bf3f6b0d2055ced022385764c988da401b
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-04-24 20:23:46 +0200

    StepThree update

--- src/ATM/ATM.Integration.Test/StepThree.cs
+++ src/ATM/ATM.Integration.Test/StepThree.cs
@@ -3,14 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ATM.Classes;
+using ATM.Classes.Boundary;
+using ATM.Classes.Domain;
+using ATM.Classes.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
+using TransponderReceiver;
 
 namespace ATM.Integration.Test
 {
     [TestFixture()]
     public class StepThree
     {
-        
+        // Driver
+        private TrackObservationSystem trackObservationSystem;
+
+        // Stub
+        private Classes.Interfaces.ILogger consoleLogger;
+        private ITransponderReceiver transponderReceiver;
+
+        // Included
+        private IOutput output;
+        private ITrackRepository trackRepository;
+        private ISeperation seperation;
+        private ILogHelper logHelper;
+        private Classes.Interfaces.ILogger eventLogger;
+        private ITrack track;
+        private SeperationEventArgs seperationEventArgs;
+        private IAirSpace airspace;
+
+
+        [SetUp]
+        public void Init()
+        {
+            consoleLogger = Substitute.For<Classes.Interfaces.ILogger>();
+            output = new Output();
+            trackRepository = new TrackRepository();
+            seperation = new Seperation();
+            logHelper = new LogHelper();
+            logHelper.Logger = consoleLogger;
+            eventLogger = new EventLogger();
+            output.LogHelper = logHelper;
+            airspace = new AirSpace();
+            transponderReceiver = Substitute.For<ITransponderReceiver>();
+
+            trackObservationSystem = new TrackObservationSystem(transponderReceiver);
+            trackObservationSystem.TrackRepository = trackRepository;
+            trackObservationSystem.Output = output;
+            trackObservationSystem.AirSpace = AirSpace;
+
+
+        }
     }
 }
