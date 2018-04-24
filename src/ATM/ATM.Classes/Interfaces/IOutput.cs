5d096627482e9ac5de6e044050612b64f65f6eb9
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-04-24 17:49:48 +0200

    Pushing diagram

--- src/ATM/ATM.Classes/Interfaces/IOutput.cs
+++ src/ATM/ATM.Classes/Interfaces/IOutput.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using ATM.Classes.Boundary;
 using ATM.Classes.Domain;
 
 namespace ATM.Classes.Interfaces
 {
     public interface IOutput
     {
+        ILogHelper LogHelper { get; set; }
+
         void Print(IEnumerable<ITrack> tracks);
         void SeperationAlert(ISeperationAlert seperationAlert);
     }
